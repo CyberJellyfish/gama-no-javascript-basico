51bce122fb960edd35ebbe2ae8081550775b4417
Yagnes Salomão <yrsalomao@gmail.com>
2020-08-23 22:20:56 -0300

    Criada a função validaCPF e a variável resultadoValidacao

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 console.log('Jacascript carregado');
 
+function validaCPF(cpf){
+    return true;
+}
+
 function validacao(){
     console.log('Iniciando validação CPF');
 
     var cpf = document.getElementById('cpf_digitado').value;
-    console.log('cpf')
+    console.log('cpf');
+
+    var resultadoValidacao = validaCPF(cpf);
+
+    if (resultadoValidacao){
+        document.getElementById('success').style.display = 'block';
+    }
 }
 
 
