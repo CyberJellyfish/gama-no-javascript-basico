2cbbc9f9a7b5143978f2376d341c17378727c3b8
Yagnes Salomão <yrsalomao@gmail.com>
2020-08-23 21:58:07 -0300

    Inserida a função validação e a variável CPF

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log('Jacascript carregado');
+
+function validacao(){
+    console.log('Iniciando validação CPF');
+
+    var cpf = document.getElementById('cpf_digitado').value;
+    console.log('cpf')
+}
+
+
